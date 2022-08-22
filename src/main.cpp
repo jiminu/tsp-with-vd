83cacd47978e2971f21788937c8df2b656401e72
jiminu <jmu417@gmail.com>
2022-08-22 17:43:16 +0900

    move

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,4 @@ int main(int, char**) {
     HeuristicAlgorithm heuristic;
     
     std::cout << "Hello, world!\n";
-    // int size = 100000;
-    
-    // int** a = new int*[size];
-
-    // for (int i = 0; i < size; ++i) {
-    //     a[i] = new int[size];
-    // }
-
-    // for (int i = 0; i < size; ++i) {
-    //     for (int j = 0; j < size; ++j) {
-    //         a[i][j] = 10;
-    //         // std::cout << a[i][j] << std::endl;
-    //     }
-    // }
-    // int b = 0;
 }
